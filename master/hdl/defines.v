616030bbe3354f9bc1da7673af23d66dd6db9f01
王云鹏 <597045367@qq.com>
2016-12-13 11:32:02 +0800

    12.13

--- master/hdl/defines.v
+++ master/hdl/defines.v
@@ -1,13 +1,51 @@
 `define	CANoP	40'h397737c573
-`define	SAvEd	40'h6d771c795e
 `define	startup	1	
 `define	flash		2
 `define	constant	3
 `define	mode		4
-`define	alarm		5
-`define	r_only	6
-`define	r_onLy	7
+
 `define	set		8
 `define	main		9
 `define	single_flash	10
 `define	monitor	11
+
+//4.3.1 状态显示
+
+`define	SAvEd	40'h6d771c795e
+`define	r_only	6
+·define	locked
+`define	outofrange
+`define	sv_on
+`define	po_on
+`define	alarm		5
+
+//shift hold for 2s to change +/_
+
+//4.3.5  P0_2  [0:25]
+`define	fbpuu
+`define	c_puu
+`define	erpuu
+`define	fbpls
+`define	c_pls
+`define	erpls
+`define	cp_fr
+`define	speed
+`define	cspd1
+`define	cspd2
+`define	c_tq1
+`define	c_tq2
+`define	aug_l
+`define	pe_l
+`define	u_bus
+`define	j_l
+`define	igbtt
+`define	rsnfr
+`define	d1ffz
+`define	nnap1
+`define	nnap2
+`define	nnap3
+`define	nnap4
+`define	uar_1
+`define	uar_2
+`define	uar_3
+`define	uar_4
--- master/hdl/interface.v
+++ master/hdl/interface.v
@@ -3,6 +3,7 @@
 module	interface(
 	input		wire			clk,			//slow clk, 20M
 	input		wire			scan_clk,	//1MHZ
+	input		wire			flash_clk,	//10HZ
 	input		wire			reset,
 	
 	input		wire			key_mode,
@@ -11,28 +12,18 @@ module	interface(
 	input		wire			key_shift,
 	input		wire			key_set,
 	
-	input		wire[2:0]	flash_cnt,
-	
-	input		wire			hold_flag,
-	
 	output	reg[3:0]		seg_select,
 	output	reg[2:0]		seg_mode,
 	output	reg[7:0]		seg_out
 );
 
-reg[39:0]			monitors[0:25];
-reg[]					monitor_value[0:25];
+reg[39:0]	monitors[0:25];
+reg[20:0]	monitor_value[0:25];			//unknown max number
 
 reg[10:0]	mode_addr;		//format:P(mode[10:8]) - mode_addr[7:0]	eg: mode_addr = 11'h701 => P7-01
 reg[9:0]		alm;
 
-/*reg			key_minus_set;
-reg			key_mode_set;
-reg			key_plus_set;
-reg			key_set_set;
-reg			key_shift_set;*/
-
-reg[2:0]		keys_set;		//3'h0:set;3'h1:shift3'h2:mode;3'h3:plus;3'h4:minus;3'h5:plus_hold;3'h6:minus_hold.
+reg[2:0]		keys_set;		//3'h1:set;3'h2:shift3'h3:mode;3'h4:plus;3'h5:minus;3'h6:plus_hold;3'h7:minus_hold.
 reg[12:0]	hold_cnt;
 
 reg[39:0]	data_out;
@@ -76,16 +67,8 @@ end
 endfunction
 
 //create 8 tables to save register mode, 3 bits for each
-reg[191:0]	rm_table0=192'h80000092001da18024924804924920024920000000000918;
-reg[266:0]	rm_table1=267'h59972be04f73e2a7ede36fec5080c5261ac36b069fb5e137dc0442a67ed87a8b051c71c5d9;
-reg[242:0]	rm_table2=243'hefd56a6fbb6f8cb7467f7091406b2b2147eedb31cff2a45dc2b11e5143bd67be61c;
-reg[38:0]	rm_table3=39'h40ac78323da;
-reg[74:0]	rm_table4=75'h3ad25d0ec7255a9a87780;
-reg[299:0]	rm_table5=300'h81c103907099aade705acf91d23f01e67ee458e6df6027651c80c72aecefb3da0b62f10380809aa86e0;
-reg[299:0]	rm_table6=300'h820bebb5e8fc5bdcc11905778054fcbe9e3cd640bc5a132c875a014b15c71c71c71c71c71c71c71c71c;
-reg[83:0]	rm_table7=84'he5c5bb81b9eabb95c71c71c;
 
-always	@	(posedge clk)	begin		//data to be displayed
+always	@	(posedge clk or negedge reset)	begin		//data to be displayed
 	if(!reset)	begin
 		data_out				<=	32'h0;
 		seg_mode				<=	`constant;
@@ -126,14 +109,19 @@ always	@	(posedge clk)	begin		//data to be displayed
 	end
 end
 
-always	@	(posedge	clk)	begin		//state control
+always	@	(posedge	clk or negedge reset)	begin		//state control
 	if(!reset)	begin
 		state						<=	`main;
+		monitor_add				<=	5'h0;
 	end	else	begin
 		case(state)
 			`startup:begin
-				if(flash_cnt == 3'd4)	begin
+				if(delay_cnt == 5'h14)	begin
 					state			<=	`main;
+					delay_cnt	<=	5'h0;
+				end	else	begin
+					delay_cnt	<=	delay_cnt + 1'b1;
+					state			<=	`startup;
 				end
 			end
 			`mode:begin
@@ -152,7 +140,7 @@ always	@	(posedge	clk)	begin		//state control
 						if(monitor_add == 5'h1A)	begin
 							monitor_add	<=	5'h0;
 						end	else	begin
-							monitor_add	<=	monitor_add + 1;
+							monitor_add	<=	monitor_add + 1'b1;
 						end
 					end
 					3'h4:begin						//key_minus
@@ -160,7 +148,7 @@ always	@	(posedge	clk)	begin		//state control
 						if(monitor_add == 5'h0)	begin
 							monitor_add	<=	5'h1A;
 						end	else	begin
-							monitor_add	<=	monitor_add - 1;
+							monitor_add	<=	monitor_add - 1'b1;
 						end
 					end
 					//3'h5:						//key_plus_hold			//useless in mode main
@@ -185,13 +173,17 @@ always	@	(posedge	clk)	begin		//state control
 	end
 end
 
-always	@	(posedge	clk)	begin		//key_hold		10ms
+always	@	(posedge	clk or negedge reset)	begin		//key_hold		10ms
+	if(!reset)	begin
+		hold_cnt		<=	13'h0;
+		hold_flag	<=	1'b0;
+	end	else	
 	if(hold_cnt == 13'h2710)	begin
 		hold_cnt		<=	13'h0;
 		hold_flag	<=	1'b1;
 	end	else	
 	if(key_minus || key_plus)	begin
-		hold_cnt		<=	hold_cnt	+ 1;
+		hold_cnt		<=	hold_cnt	+ 1'b1;
 		hold_flag	<=	1'b0;
 	end	else	begin
 		hold_cnt		<=	13'h0;
@@ -199,8 +191,13 @@ always	@	(posedge	clk)	begin		//key_hold		10ms
 	end
 end
 
+/*to be rewrite*/
+/*capture at posedge. use old hold solution*/
 
-always	@	(posedge	scan_clk)	begin		//key scan, use scan_clk
+always	@	(posedge	scan_clk or negedge reset)	begin		//key scan, use scan_clk
+	if(!reset)	begin
+		keys_set		<=	3'h0;
+	end	else
 	if(key_mode)	begin
 		if(key_mode)	begin
 			keys_set	<=	3'h3;
@@ -217,16 +214,19 @@ always	@	(posedge	scan_clk)	begin		//key scan, use scan_clk
 	end	else	
 	if(key_plus)	begin
 		if(key_plus)	begin
-			if(hold_flag)	begin
-				keys
+			if(hold_flag)	begin				//plus_hold
+				keys_set	<=	3'h6;
 			end	else	begin
-			keys_set	<=	3'h4;
+				keys_set	<=	3'h4;
 		end	else	begin
 			keys_set	<=	3'h0;
 		end
 	end	else	
 	if(key_minus)	begin
 		if(key_minus)	begin
+			if(hold_flag)	begin				//minus_hold
+				keys_set	<=	3'h7;
+			end	else	begin
 			keys_set	<=	3'h5;
 		end	else	begin
 			keys_set <=	3'h0;
