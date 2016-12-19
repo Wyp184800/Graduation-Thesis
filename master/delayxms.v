f53d36ca7d456a59f2d51ddd6bf10ea72c3b99ff
王云鹏 <597045367@qq.com>
2016-12-19 21:56:30 +0800

    还是写完了这个模块……

--- master/delayxms.v
+++ master/delayxms.v
@@ -1,5 +1,8 @@
+/*功耗优化：输入使能信号只控制一个always块，拓宽输入信号宽度*/
+
 module	delayxms(
-	input		wire			clk,		//20MHZ
+	input		wire			clk,
+	input		wire			scan_clk,		//1MHZ
 	input		wire			reset,
 	
 	input		wire			enable_i,
@@ -10,32 +13,75 @@ module	delayxms(
 
 reg[3:0]		ready_t;
 reg[3:0]		done_t;
-reg[0:20]	cnt[0:3];
+reg[20:0]	cnt[0:3];
 
-always	@	(posedge	clk or negedge rst)	begin		//10ms
+always	@	(posedge	scan_clk or negedge rst)	begin		//10ms
 	if(!reset)	begin
 		ready_t[0]		<=	1'b0;
-	end	else	
-	if(enable_i)	begin
-		if(done_t[0] == 1'b0)	begin
-			if(cnt[0] == 18'd200000)	begin
-				cnt[0]		<=	18'b0;
-				done_t[0]	<= 1'b1;
-				ready_t[0]	<=	1'b1;
+	end	else	begin
+		if(enable_i)	begin
+			if(done_t[0] == 1'b0)	begin
+				if(cnt[0] == 17'd10000)	begin
+					cnt[0]		<=	18'b0;
+					done_t[0]	<= 1'b1;
+					ready_t[0]	<=	1'b1;
+				end	else	begin
+					cnt[0]		<=	cnt + 1'b1;
+					done_t[0]	<=	1'b0;
+					ready_t[0]	<=	1'b0;
+				end
 			end	else	begin
-				cnt[0]		<=	cnt + 1'b1;
-				done_t[0]	<=	1'b0;
-				ready_t[0]	<=	1'b0;
+				cnt[0]			<=	18'b0;
+				done_t[0]		<=	1'b0;
+				ready_t[0]		<=	1'b0;
 			end
 		end	else	begin
-			cnt[0]			<=	18'b0;
-			done_t[0]		<=	1'b0;
-			ready_t[0]		<=	1'b0;
+			cnt[0]				<=	18'b0;
+			done_t[0]			<=	1'b0;
+			ready_t[0]			<=	1'b0;
 		end
+	end
+end
+
+always	@	(posedge scan_clk or negedge reset)	begin		//2s
+	if(!reset)	begin
+		ready_t[1]		<=	1'b0;
 	end	else	begin
-		cnt[0]				<=	18'b0;
-		done_t[0]			<=	1'b0;
-		ready_t[0]			<=	1'b0;
+		if(enable_i)	begin
+			if(done_t[1] == 1'b0)	begin
+				if(cnt[1] == 21'd2000000)	begin
+					cnt[1]		<=	21'b0;
+					done_t[1]	<= 1'b1;
+					ready_t[1]	<=	1'b1;
+				end	else	begin
+					cnt[1]		<=	cnt + 1'b1;
+					done_t[1]	<=	1'b0;
+					ready_t[1]	<=	1'b0;
+				end
+			end	else	begin
+				cnt[1]			<=	21'b0;
+				done_t[1]		<=	1'b0;
+				ready_t[1]		<=	1'b0;
+			end
+		end	else	begin
+			cnt[1]				<=	21'b0;
+			done_t[1]			<=	1'b0;
+			ready_t[1]			<=	1'b0;
+		end
+	end
+end
+
+always	@	(posedge	clk or negedge reset)	begin
+	if(!reset)	begin
+		ready	<=	1'b0;
+	end	else	begin
+		case(x)
+			2'b00:	ready	<=	ready_t[0];
+			2'b01:	ready	<=	ready_t[1];
+			2'b10:	ready	<=	ready_t[2];
+			2'b11:	ready	<=	ready_t[3];
+			default:	ready	<=	1'b0;
+		endcase
 	end
 end
 
